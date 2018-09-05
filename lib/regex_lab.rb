a9ea43feb980a5cd5a4c0a70af6b774929f7effa
Amil Masic <amil.masic@gmail.com>
2018-09-05 23:25:33 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,7 +20,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].+\.$/) 
+  if text.match(/^[A-Z].+\.$/)
     true
   else
     false
@@ -28,5 +28,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+  if phone.match (/^[(]?\d{3}[(]?[\s]?\d{3}[\s-]?\d{4}$/)
+    true
+  else
+    false
+  end
 
 end
