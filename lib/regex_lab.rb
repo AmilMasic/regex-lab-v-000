7d52e38aaa8b436e372ae0fa1671af07a7384a9d
Amil Masic <amil.masic@gmail.com>
2018-09-05 22:03:22 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,22 @@
+# require "pry"
 def starts_with_a_vowel?(word)
-
+  # binding.pry
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else
+     false
+    end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+   # # text.scan(/\w^un+\w$ing/)
+   text.scan(/\^un/)
+
 
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/^[a-zA-Z]{5}$/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
